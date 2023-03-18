90bff9a68de300acbb9c93fa5e54f935a853f6a7
Douglas Santos <douglas.santos.rodriguez@gmail.com>
2023-03-18 20:54:55 -0300

    feat:adicionado react route dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from './routes';
 
 function App() {
   return (
-    <div className="App">
-   
-        
-        <p>
-          ola
-        </p>
-     
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export {}
+import {Routes, Route, Navigate} from 'react-router-dom';
+
+export default function AppRoutes(){
+    return(
+        <Routes>
+            <Route path='/pagina-inicial' element={<p> Pagina Inicial</p>}/>
+            <Route path='*' element={<Navigate to="/pagina-inicial"/>}/>
+        </Routes>
+
+    );
+
+
+}
