675ebe62a4a9223a650954f065ef3f8c4080d669
Douglas Santos <douglas.santos.rodriguez@gmail.com>
2023-03-18 21:34:39 -0300

    feat: tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import {ThemeProvider} from '@mui/material';
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import AppRoutes from './routes';
+import { LightTheme } from './shared/themes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
